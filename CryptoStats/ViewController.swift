5b834feca9ee4a2d9b68e41302cec923c1b0b493
Max Sashcheka <sashchekam@gmail.com>
2022-02-08 22:53:40 +0300

    Initial Commit

---
+++ CryptoStats/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoStats
+//
+//  Created by Max Sashcheka on 8.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
