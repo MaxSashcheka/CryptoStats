3234e05dc9c31da4ac11e9de86cba9130020bf5b
maksim.sashcheka <maksim.sashcheka@yellow.systems>
2022-02-14 22:58:40 +0300

    Implemented CoinsInteractor and changed DI for coins table screen

---
+++ CryptoStats/Core/Interactors/Coins/CoinsInteractorProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  CoinsInteractorProtocol.swift
+//  CryptoStats
+//
+//  Created by Max Sashcheka on 14.02.22.
+//
+
+import Foundation
---
+++ CryptoStats/Screens/Settings/SettingsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SettingsViewModel.swift
+//  CryptoStats
+//
+//  Created by Max Sashcheka on 13.02.22.
+//
+
+import Foundation
